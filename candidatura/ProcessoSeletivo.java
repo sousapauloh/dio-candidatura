79d21b5ef4b8939943e3337ef6b9122534ce2794
Paulo Henrique <sousa.pauloh@gmail.com>
2023-09-22 11:34:37 -0300

    q

--- candidatura/ProcessoSeletivo.java
+++ candidatura/ProcessoSeletivo.java
@@ -1,5 +1,22 @@
 package candidatura;
 
 public class ProcessoSeletivo {
-    
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+    // Criando metodo estatico que nao vai retornar nenhuma opção
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA CANDIDATO");
+        } else if (salarioBase == salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+
+    }
 }
