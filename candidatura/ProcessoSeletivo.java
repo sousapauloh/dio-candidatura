0a99c71ea991ca5c2de73a89f6352155e4e3e4f1
Paulo Henrique <sousa.pauloh@gmail.com>
2023-09-28 10:41:57 -0300

    b

--- candidatura/ProcessoSeletivo.java
+++ candidatura/ProcessoSeletivo.java
@@ -1,10 +1,76 @@
+//Separando em pacotes os programas.
 package candidatura;
+//importando biblioteca de sorteio aleatório
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+        imprimirSelecionados();
+        String [] candidatos = {"FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO"};
+        for(String candidato: candidatos)
+        entrandoEmContato(candidato);
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+        System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+
+        else
+        System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NÚMERO MÁXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADAS");
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+        for(int indice = 0; indice < candidatos.length; indice++ ) {
+            System.out.println("O candidato de nº " + (indice +1) + " é " + candidatos[indice]);
+        }
+        
+    }
+
+    // metodo para ligação se o valor 1==3 ficar =1 ele atendeu
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    //
+    static void selecaoCandidatos() {
+        String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO", "MÔNICA", "FABRÍCIO", "MIRELA",
+                "DANIELA", "JORGE" };
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     // Criando metodo estatico que nao vai retornar nenhuma opção
